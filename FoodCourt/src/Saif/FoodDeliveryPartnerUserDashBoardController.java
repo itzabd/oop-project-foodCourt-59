7c85c2d3be8a9d956a94bcaacfc6a1353560bb04
Saif Nasim Bosunia <101852568+SaifNasim@users.noreply.github.com>
2024-04-01 20:35:55 +0600

    FoodDeliveryPartner and Stall manager

---
+++ FoodCourt/src/Saif/FoodDeliveryPartnerUserDashBoardController.java
@@ -0,0 +1,61 @@
+
+package Saif;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class FoodDeliveryPartnerUserDashBoardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void editProfileMenuItem(ActionEvent event) {
+    }
+
+    @FXML
+    private void logOutMenuItem(ActionEvent event) {
+    }
+
+    @FXML
+    private void reviewContractButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void checkDeliveryButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void shareDeliveryPartnerButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void notifyCustomerButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void applyForEquipmentButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void generateIncomeButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void applicationButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void reportButton(ActionEvent event) {
+    }
+    
+}
