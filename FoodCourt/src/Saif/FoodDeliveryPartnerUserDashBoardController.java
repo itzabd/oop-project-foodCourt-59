9a6d2089be43b366306ad0ce845d361f20f0e73b
Saif Nasim Bosunia <101852568+SaifNasim@users.noreply.github.com>
2024-04-07 01:36:56 +0600

    updateButtonColoring
    just colored AND uncolored  the buttons for the user to get which button is selected

--- FoodCourt/src/Saif/FoodDeliveryPartnerUserDashBoardController.java
+++ FoodCourt/src/Saif/FoodDeliveryPartnerUserDashBoardController.java
@@ -1,14 +1,34 @@
 
 package Saif;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.Button;
+
 
 public class FoodDeliveryPartnerUserDashBoardController implements Initializable {
+
+    @FXML
+    private Button reviewContractButtonId;
+    @FXML
+    private Button checkDeliveryButtonId;
+    @FXML
+    private Button shareDeliveryPartnerButtonId;
+    @FXML
+    private Button notifyCustomerButtonId;
+    @FXML
+    private Button applyForEquipmentButtonId;
+    @FXML
+    private Button generateIncomeButtonId;
+    @FXML
+    private Button applicationButtonId;
+    @FXML
+    private Button reportButtonId;
 
     /**
      * Initializes the controller class.
@@ -20,6 +40,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void editProfileMenuItem(ActionEvent event) {
+        
     }
 
     @FXML
@@ -28,34 +49,118 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void reviewContractButton(ActionEvent event) {
+
+        reviewContractButtonId.setStyle("-fx-background-color : #98a2b0;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4" );
     }
 
     @FXML
     private void checkDeliveryButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #98a2b0;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        
     }
 
     @FXML
     private void shareDeliveryPartnerButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #98a2b0;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+               
     }
 
     @FXML
     private void notifyCustomerButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #98a2b0;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+   
     }
 
     @FXML
     private void applyForEquipmentButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #98a2b0;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+       
     }
 
     @FXML
     private void generateIncomeButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #98a2b0;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+   
     }
 
     @FXML
     private void applicationButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #98a2b0;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+   
     }
 
     @FXML
     private void reportButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #98a2b0;" );
+   
+    }
+
+    @FXML
+    private void backButton(ActionEvent event) {
+        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applyForEquipmentButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
+        generateIncomeButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
+        applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
+        reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
     }
     
 }
