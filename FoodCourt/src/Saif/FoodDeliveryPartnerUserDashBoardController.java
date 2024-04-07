561b103d9223a5c9896b0330f075d3b41106b99a
Saif Nasim Bosunia <101852568+SaifNasim@users.noreply.github.com>
2024-04-07 23:25:13 +0600

    arekbar commit

--- FoodCourt/src/Saif/FoodDeliveryPartnerUserDashBoardController.java
+++ FoodCourt/src/Saif/FoodDeliveryPartnerUserDashBoardController.java
@@ -4,17 +4,22 @@ package Saif;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 
 public class FoodDeliveryPartnerUserDashBoardController implements Initializable {
 
-    @FXML
-    private Button reviewContractButtonId;
+    
     @FXML
     private Button checkDeliveryButtonId;
     @FXML
@@ -29,13 +34,46 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
     private Button applicationButtonId;
     @FXML
     private Button reportButtonId;
+    @FXML
+    private AnchorPane contractAPane;
+    @FXML
+    private AnchorPane checkDeliveryAPane;
+    @FXML
+    private TextField contractFDPName;
+    @FXML
+    private TextField contractFDPNumber;
+    @FXML
+    private TextField contractFDPEmail;
+    @FXML
+    private ComboBox<String> contractFDPComboBDeliveryType;
+    @FXML
+    private ComboBox<String> contractFDPComboBDeliveryShift;
+    @FXML
+    private DatePicker contractFDPDatePStart;
+    @FXML
+    private DatePicker contractFDPDatePEnd;
+    @FXML
+    private Button renewContractButtonId;
+    @FXML
+    private Button loadInTableButtonId;
+    @FXML
+    private Button submitButtonId;
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        contractAPane.setVisible(false);
+        checkDeliveryAPane.setVisible(false);
+        
+        ObservableList deliveryType = FXCollections.observableArrayList("By Walking", "By Cycling", "By biking");
+        contractFDPComboBDeliveryType.setItems(deliveryType);
+        
+        ObservableList deliveryShift = FXCollections.observableArrayList("Morning","Noon","Evening","Night");
+        contractFDPComboBDeliveryShift.setItems(deliveryShift);
+    
     }    
 
     @FXML
@@ -48,9 +86,13 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
     }
 
     @FXML
-    private void reviewContractButton(ActionEvent event) {
-
-        reviewContractButtonId.setStyle("-fx-background-color : #98a2b0;");
+    private void renewContractButton(ActionEvent event) {
+        // Anchor Pane visibility ..
+        contractAPane.setVisible(true);
+        checkDeliveryAPane.setVisible(false);
+        
+        // button color changing .. 
+        renewContractButtonId.setStyle("-fx-background-color : #98a2b0;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4");    
@@ -62,7 +104,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void checkDeliveryButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #98a2b0;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -75,7 +117,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void shareDeliveryPartnerButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #98a2b0;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -88,7 +130,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void notifyCustomerButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #98a2b0;");    
@@ -101,7 +143,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void applyForEquipmentButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -114,7 +156,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void generateIncomeButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -127,7 +169,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void applicationButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -140,7 +182,7 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void reportButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -153,7 +195,13 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
 
     @FXML
     private void backButton(ActionEvent event) {
-        reviewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
+        
+        // Anchor visibility ..
+        contractAPane.setVisible(false);
+        checkDeliveryAPane.setVisible(false);
+        
+        // button color changing..
+        renewContractButtonId.setStyle("-fx-background-color : #a7bfe4;");
         checkDeliveryButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         shareDeliveryPartnerButtonId.setStyle("-fx-background-color : #a7bfe4;" );    
         notifyCustomerButtonId.setStyle("-fx-background-color :  #a7bfe4;");    
@@ -162,5 +210,17 @@ public class FoodDeliveryPartnerUserDashBoardController implements Initializable
         applicationButtonId.setStyle("-fx-background-color : #a7bfe4;" );
         reportButtonId.setStyle("-fx-background-color : #a7bfe4;" );
     }
+
+    @FXML
+    private void loadInTableButton(ActionEvent event) {
+        loadInTableButtonId.setStyle("-fx-background-color : #98a2b0;" );
+    }
+
+    @FXML
+    private void submitButton(ActionEvent event) {
+        loadInTableButtonId.setStyle("-fx-background-color : #b4c6fa;");
+        
+    }
+
     
 }
